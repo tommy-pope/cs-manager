641fc69d50c8d1ad17865d8089b2c91e68fd51aa
tommy-pope <tommypope20@gmail.com>
2023-08-27 16:51:38 -0500

    WIP team generation

--- src/data/continent/continent.py
+++ src/data/continent/continent.py
@@ -3,5 +3,19 @@ class Continent:
         self.name = continent_name
         self.sname = short_name
         self.rep = continent_rep
-        self.nations = []
+        self.nations = {}
         self.rankings = {}
+
+        self.nation_distribution = {}
+    
+    def calculate_nation_distribution(self) -> None:
+        # normalize, calculate percentage using power function, normalize weights, convert weights to percentages
+        nations = [nation for nation in self.nations.values()]
+
+        normalized_reps = [value.rep / 100 for value in nations]
+        weights = [value**3 for value in normalized_reps]
+        normalized_weights = [weight / sum(weights) for weight in weights]
+        percentages = [weight * 100 for weight in normalized_weights]
+
+        for idx in range(len(percentages)):
+            self.nation_distribution[nations[idx].sname] = percentages[idx]
--- src/gamefuncs/db.py
+++ src/gamefuncs/db.py
@@ -1,8 +1,10 @@
 from ..data.continent.continent import Continent
 from ..data.nation.nation import Nation
 
+from ..data.player.player import Player
+
 import csv
-import os
+import random
 from pathlib import Path
 
 class GameDB:
@@ -12,6 +14,8 @@ class GameDB:
         self.teams = []
         self.players = []
         self.events = []
+
+        self.continents_distribution = {}
     
     def setup_game(self) -> None:
         self.generate_continents()
@@ -29,10 +33,27 @@ class GameDB:
         self.continents["SA"] = sa
         self.continents["ASIA"] = asia
 
+        self.calculate_continent_distribution()
+
+    def calculate_continent_distribution(self) -> None:
+        for continent in self.continents:
+            cont_obj = self.continents[continent]
+            reputation = cont_obj.rep
+            percentages = []
+
+            for i in range(5):
+                percentage = ((reputation/100) ** 3) / (i + 1)
+                percentages.append(percentage)
+
+            print(f"{continent}: {percentages}")
+
+
+
+
+
     def generate_nations(self) -> None:
         fp = Path("src/data/nation/nations.csv")
         with open(fp, "r") as file:
-
             reader = csv.reader(file, delimiter=",")
             for line in reader:
                 name = line[0]
@@ -41,8 +62,10 @@ class GameDB:
                 continent = line[3]
                 
                 nation = Nation(name, sname, rep, continent)
-                self.nations.append(nation)
-                self.continents[continent].nations.append(nation)
+                self.continents[continent].nations[sname] = nation
+
+        for continent in self.continents.values():
+            continent.calculate_nation_distribution()
 
     def generate_teams(self) -> None:
         eu_teams = 0
@@ -52,4 +75,31 @@ class GameDB:
 
         for teams in range(na_teams):
 
-    def generate_team(self, nation: Nation):
+
+
+
+            self.generate_team(self.continents["NA"])
+
+    def generate_team(self, continent: Continent):
+        
+
+        players = []
+
+        while len(players) < 5:
+            # no previous players generated
+            if len(players) == 0:
+                nation_chance = random.uniform(0, 1) * 100
+                cumulative_weight = 0
+                nation = None
+
+                for name in continent.nation_distribution:
+                    cumulative_weight += continent.nation_distribution[name] / 100
+                    if nation_chance <= cumulative_weight:
+                        nation = continent.nations[name]
+                        break
+
+                player = self.generate_player(nation)
+                players.append(player)
+
+    def generate_player(self, nation: Nation) -> Player:
+        pass
